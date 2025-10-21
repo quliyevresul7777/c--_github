503f06a91a089cbfcb3a6fe06162df1a69570ef2
quliyevresul7777 <quliyev.resul7777@gmail.com>
2025-10-21 09:05:10 +0400

    Update codeee3.5.cpp

--- codeee3.5.cpp
+++ codeee3.5.cpp
@@ -1,22 +1,14 @@
+
 #include <iostream>
-#include <cmath>
-
+using namespace std;
 int main() {
-    long long x;
-    std::cout << "Eded daxil edin: ";
-    if (!(std::cin >> x)) {
-        std::cerr << "Sehv giris\n";
-        return 1;
+    int n, cem = 0;
+    cin >> n;
+    n = abs(n);
+    while (n > 0) {
+        cem += n % 10;
+        n /= 10;
     }
-
-    x = std::llabs(x); // mənfi ədədi müsbətə çevir
-    int sum = 0;
-    if (x == 0) sum = 0;
-    while (x > 0) {
-        sum += x % 10;
-        x /= 10;
-    }
-
-    std::cout << "Reqemlerinin cemi: " << sum << "\n";
+    cout << cem;
     return 0;
 }
